bf307390a7ba0a199186944985da7f93c84239b1
Alex Morales <almoral>
2016-07-19 14:51:16 -0400

    Cleanup (Removed routing code)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { MayorPdfSearchAppComponent, environment } from './app/'
-import {APP_ROUTER_PROVIDER} from './app/routes'
+
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(MayorPdfSearchAppComponent, [ APP_ROUTER_PROVIDER ]);
+bootstrap(MayorPdfSearchAppComponent);
