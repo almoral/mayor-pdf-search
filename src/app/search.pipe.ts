ba7e47b4f03acee82a8e1dece290bae25d6dd61e
almoral <AlexMorales@Alejandros-MacBook-Pro-3.local>
2016-07-20 11:06:18 -0400

    Using target instead of path to get the value from the dropdowns. This fixes an issue in Firefox where they weren't working.

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -18,31 +18,31 @@ transform(value: any, term:string, selectedYear:any, selectedMonth:any){
 		
 			if(!selectedYear && !selectedMonth) {
 
-				return value.filter((item:any) => item.year !== null && item.year == selectedYear.path[0].value);
+				return value.filter((item:any) => item.year !== null && item.year == selectedYear.target.value);
 			}
 
 		
 			if(selectedYear && !selectedMonth){
 
-				return value.filter((item:any) => item.year !== null && item.year == selectedYear.path[0].value && item.label.toLowerCase().indexOf(term.toLowerCase()) > -1);
+				return value.filter((item:any) => item.year !== null && item.year == selectedYear.target.value && item.label.toLowerCase().indexOf(term.toLowerCase()) > -1);
 
 			}
 
 			if(selectedYear && selectedMonth){
 
-				return value.filter((item:any) => item.year !== null && item.year == selectedYear.path[0].value && item.label.toLowerCase().indexOf(term.toLowerCase()) > -1 && item.monthLabel == selectedMonth.path[0].value);
+				return value.filter((item:any) => item.year !== null && item.year == selectedYear.target.value && item.label.toLowerCase().indexOf(term.toLowerCase()) > -1 && item.monthLabel == selectedMonth.target.value);
 
 			}
 
 			if(!selectedYear && selectedMonth){
 
-				return value.filter((item:any) => item.year !== null && item.monthLabel == selectedMonth.path[0].value);
+				return value.filter((item:any) => item.year !== null && item.monthLabel == selectedMonth.target.value);
 
 			}
 
 			if(selectedYear && selectedMonth){
 
-				return value.filter((item:any) => item.year !== null && item.label.toLowerCase().indexOf(term.toLowerCase()) > -1 && item.monthLabel == selectedMonth.path[0].value);
+				return value.filter((item:any) => item.year !== null && item.label.toLowerCase().indexOf(term.toLowerCase()) > -1 && item.monthLabel == selectedMonth.target.value);
 
 			}
 
